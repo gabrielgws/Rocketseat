b366adcd5f15e0c8bf1d28daed754e8ad946e91e
Gabriel Willian <gabrielgws@Magrelim12.localdomain>
2022-01-24 18:25:24 -0300

    Conclusão Chapter III

--- Chapter-III/ignews/src/components/SubscribeButton/index.tsx
+++ Chapter-III/ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { useSession, signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { api } from '../../services/aip';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface subscribeButtonProps {
@@ -5,10 +10,40 @@ interface subscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: subscribeButtonProps) {
+  const { data:session } = useSession();
+  const router = useRouter();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn ('github')
+      return;
+    }
+
+    if(session.activeSubscription) {
+      router.push('/post');
+      return
+    }
+
+    // criação da checkout session
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe Now
     </button>
